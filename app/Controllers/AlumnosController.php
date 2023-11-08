0721184d1866089d5f66977445fd50c7b43e3673
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-11-08 12:43:52 +0100

    Add CRUD functionality for AlumnosController

--- app/Controllers/AlumnosController.php
+++ app/Controllers/AlumnosController.php
@@ -1,18 +1,48 @@
 <?php
 
 class IncomesController {
+// Path: app/Controllers/IncomesController.php
+
+
+    /** 
+     * muestra una la lista de Alumnos de la base de datos
+     */
     public function index() {
     }
-    public function show() {
-    }
+
+    /**
+     *  
+     *  */    
     public function create() {
     }
+
+    /**
+     *  Guarda un nuevo Alumno en la base de datos
+     *  */    
     public function store() {
     }
+
+    /**
+     *  muestra un Alumno en especifico de la base de datos
+     *  */    
+    public function show() {
+    }
+
+    /**
+     * edita un Alumno en especifico de la base de datos
+     *  */    
     public function edit() {
     }
+
+    /**
+     * 
+     *  */    
     public function update() {
     }
+
+    /**
+     * elimina un Alumno en especifico de la base de datos
+     *  */    
     public function destroy() {
     }
 
