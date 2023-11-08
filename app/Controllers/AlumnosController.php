0f03fba5d1083d9ba7ca720e8eb8412be9312a32
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-11-08 11:13:48 +0100

    Refactor code to improve performance and readability.

---
+++ app/Controllers/AlumnosController.php
@@ -0,0 +1,19 @@
+<?php
+
+class IncomesController {
+    public function index() {
+    }
+    public function show() {
+    }
+    public function create() {
+    }
+    public function store() {
+    }
+    public function edit() {
+    }
+    public function update() {
+    }
+    public function destroy() {
+    }
+
+}
